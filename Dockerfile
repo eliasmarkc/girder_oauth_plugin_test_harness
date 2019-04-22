89641cdb8d489b2c1bad75a31a6d2fa3983a3dfe
eliasmarkc <elias3c@gmail.com>
2019-04-22 15:03:56 -0500

    Test agave plugin

--- Dockerfile
+++ Dockerfile
@@ -9,12 +9,10 @@ RUN apt-get -qq update && \
     rm -rf /var/lib/apt/lists/*
 
 # Install dependencies
-RUN git clone https://github.com/whole-tale/girder /girder --recurse-submodules -b stable
+RUN git clone https://github.com/eliasmarkc/girder /girder --recurse-submodules -b add-agave-support
 WORKDIR /girder
 RUN pip install -r requirements-dev.txt --no-cache-dir && \
     pip install flake8 coverage --no-cache-dir
-
-VOLUME /girder/plugins/wholetale
 
 ENV PYTHON="/usr/local/bin/python3" \
     COVERAGE="/usr/local/bin/coverage" \
@@ -23,9 +21,3 @@ ENV PYTHON="/usr/local/bin/python3" \
 COPY entrypoint.sh /
 ENTRYPOINT [ "/entrypoint.sh" ]
 CMD [ "" ]
-
-RUN rm -rf /girder/plugins/wt_data_manager /girder/plugins/globus_handler && \
-    git clone https://github.com/whole-tale/girder_wt_data_manager /girder/plugins/wt_data_manager -b stable && \
-    pip install -r plugins/wt_data_manager/requirements.txt && \
-    git clone https://github.com/whole-tale/globus_handler /girder/plugins/globus_handler && \
-    pip install -r plugins/globus_handler/requirements.txt
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #
-# Runs the test suite on the Girder Wholetale Plugin in an isolated environment
+# Runs the test suite on the Girder Oauth Plugin in an isolated environment
 #
-# Prerequisite: docker run -itd --name=test-mongo -p 27017:27017 -v $(pwd)/mongodata/test:/data/db mongo
-# Build: docker build -t wholetale/girder:test-harness
-# Usage: docker run --rm -it -v /home/ubuntu/deploy-dev/src/wholetale/:/girder/plugins/wholetale wholetale/girder:test-harness
+# Prerequisite: docker run -itd --name=test-mongo -p 27017:27017 --net=host mongo
+# Build: docker build -t eliasmarkc/girder:test-harness .
+# Usage: docker run --rm -it -v ~/projects/girder/plugins/oauth/:/girder/plugins/oauth --net=host eliasmarkc/girder:test-harness
 
 # Fail on error
 set -e
 
 # Install WholeTale plugin dependencies
-pip install -r plugins/wholetale/requirements.txt
-pip install -r plugins/wholetale/requirements-dev.txt
 pip install passlib pyOpenSSL
 
 # Run CMake
@@ -19,7 +17,7 @@ cmake \
     -DRUN_CORE_TESTS:BOOL=OFF \
     -DBUILD_JAVASCRIPT_TESTS:BOOL=OFF \
     -DJAVASCRIPT_STYLE_TESTS:BOOL=OFF \
-    -DTEST_PLUGINS:STRING=wholetale \
+    -DTEST_PLUGINS:STRING=oauth \
     -DCOVERAGE_MINIMUM_PASS:STRING=4 \
     -DPYTHON_COVERAGE=ON \
     -DPYTHON_STATIC_ANALYSIS=ON \
